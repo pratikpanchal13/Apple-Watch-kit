6303a3bc7e0d9c8692263f8866394bfc2e758569
pratikpanchal131 <indianic@ind371.local>
2017-03-24 12:57:19 +0530

    Initial commit

---
+++ AirAber/AirAber/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AirAber
+//
+//  Created by Mic Pringle on 05/08/2015.
+//  Copyright © 2015 Mic Pringle. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+}
